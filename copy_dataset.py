7cfbadc6096b513194f1239945cc43e53139f2ea
stjuliet <andy19966212@126.com>
2021-04-26 12:36:40 +0800

    fix dir bugs

--- copy_dataset.py
+++ copy_dataset.py
@@ -6,6 +6,11 @@ dataset_path_list = ["E:\\PythonCodes\\bbox3d_annotation_tools\\session0_center_
 voc_img_dir = "E:\\PythonCodes\\bbox3d_recognition_v2\\DATAdevkit\\DATA2021\\JPEGImages"
 voc_anno_dir = "E:\\PythonCodes\\bbox3d_recognition_v2\\DATAdevkit\\DATA2021\\Annotations"
 
+if not os.path.exists(voc_img_dir):
+    os.makedirs(voc_img_dir)
+if not os.path.exists(voc_anno_dir):
+    os.makedirs(voc_anno_dir)
+
 for single_dataset_path_list in dataset_path_list:
     file_list = os.listdir(single_dataset_path_list)
     for file_name in file_list:
