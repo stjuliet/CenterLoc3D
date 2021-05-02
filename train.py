c601620495d5a1512e9a3dad41e6417d5d3c15b5
stjuliet <andy19966212@126.com>
2021-05-02 15:26:29 +0800

    add earlystopping

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
+from pytorchtools import EarlyStopping
 
 from tqdm import tqdm
 from tensorboardX import SummaryWriter
@@ -194,6 +195,10 @@ if __name__ == "__main__":
 
     net = model.train()
 
+    # 设置早停
+    patience = 20  # 当验证集损失在连续20次训练周期中都没有得到降低时，停止模型训练，以防止模型过拟合
+    early_stopping = EarlyStopping(patience, verbose=True)
+
     if Cuda:
         net = torch.nn.DataParallel(model)
         cudnn.benchmark = True
@@ -255,83 +260,83 @@ if __name__ == "__main__":
         # 冻结一定部分训练
         model.freeze_backbone()
 
-        # 开始训练前展示训练样本
-        for step, data in enumerate(gen, start = 0):
-            # batch_center_reg: 中心点偏移量
-            img, calib_matrix, batch_hm, batch_center_reg, batch_vertex_reg, batch_size_reg, batch_center_mask, batch_box_perspective, batch_base_point, raw_img_w, raw_img_h = data
+        # # 开始训练前展示训练样本
+        # for step, data in enumerate(gen, start = 0):
+        #     # batch_center_reg: 中心点偏移量
+        #     img, calib_matrix, batch_hm, batch_center_reg, batch_vertex_reg, batch_size_reg, batch_center_mask, batch_box_perspective, batch_base_point, raw_img_w, raw_img_h = data
             
-            for num in range(len(img)):
-                plt.figure()
-                raw_img = img[num] # numpy格式, BGR, CHW
-                raw_img = raw_img.transpose(1, 2, 0)  # CHW->HWC
-                raw_img = np.array((raw_img * std + mean) * 255.).astype(np.uint8)
-                raw_img = cv.cvtColor(raw_img, cv.COLOR_BGR2RGB)  # RGB
+        #     for num in range(len(img)):
+        #         plt.figure()
+        #         raw_img = img[num] # numpy格式, BGR, CHW
+        #         raw_img = raw_img.transpose(1, 2, 0)  # CHW->HWC
+        #         raw_img = np.array((raw_img * std + mean) * 255.).astype(np.uint8)
+        #         raw_img = cv.cvtColor(raw_img, cv.COLOR_BGR2RGB)  # RGB
 
-                max_size=max(raw_img_w[num],raw_img_h[num])
-                minus_size=abs(raw_img_w[num]-raw_img_h[num])
+        #         max_size=max(raw_img_w[num],raw_img_h[num])
+        #         minus_size=abs(raw_img_w[num]-raw_img_h[num])
 
-                pil_raw_img=Image.fromarray(raw_img).resize((max_size,max_size)) # 返回PIL格式带灰条原图
-                draw=ImageDraw.Draw(pil_raw_img)
+        #         pil_raw_img=Image.fromarray(raw_img).resize((max_size,max_size)) # 返回PIL格式带灰条原图
+        #         draw=ImageDraw.Draw(pil_raw_img)
 
-                # 绘制中心点
-                t_list = np.where(batch_center_mask[num] == 1.0)  # 遍历图像域中所有目标点
-                for y, x in zip(t_list[0], t_list[1]):
-                    # 显示到带灰条原图上
-                    center = ([x, y] + batch_center_reg[num, y, x]) * max_size // output_shape[0]
-                    vertex = batch_vertex_reg[num, y, x] * max_size // output_shape[0]
+        #         # 绘制中心点
+        #         t_list = np.where(batch_center_mask[num] == 1.0)  # 遍历图像域中所有目标点
+        #         for y, x in zip(t_list[0], t_list[1]):
+        #             # 显示到带灰条原图上
+        #             center = ([x, y] + batch_center_reg[num, y, x]) * max_size // output_shape[0]
+        #             vertex = batch_vertex_reg[num, y, x] * max_size // output_shape[0]
 
-                    draw.ellipse([center[0], center[1], center[0]+5, center[1]+5], outline=(0, 0, 255), width = 1)
-                    cls_id = np.argmax(batch_hm[num, y, x])
-                    cls_name = class_names[cls_id]
-                    draw.text([center[0], center[1]-10], cls_name, fill=(255, 0, 0))
+        #             draw.ellipse([center[0], center[1], center[0]+5, center[1]+5], outline=(0, 0, 255), width = 1)
+        #             cls_id = np.argmax(batch_hm[num, y, x])
+        #             cls_name = class_names[cls_id]
+        #             draw.text([center[0], center[1]-10], cls_name, fill=(255, 0, 0))
 
-                    # 宽度方向
-                    # 0-1  2-3  4-5  6-7
-                    draw.line([vertex[0], vertex[1], vertex[2], vertex[3]], fill=128, width=2)
-                    draw.line([vertex[4], vertex[5], vertex[6], vertex[7]], fill=128, width=2)
-                    draw.line([vertex[8], vertex[9], vertex[10], vertex[11]], fill=128, width=2)
-                    draw.line([vertex[12], vertex[13], vertex[14], vertex[15]], fill=128, width=2)
+        #             # 宽度方向
+        #             # 0-1  2-3  4-5  6-7
+        #             draw.line([vertex[0], vertex[1], vertex[2], vertex[3]], fill=128, width=2)
+        #             draw.line([vertex[4], vertex[5], vertex[6], vertex[7]], fill=128, width=2)
+        #             draw.line([vertex[8], vertex[9], vertex[10], vertex[11]], fill=128, width=2)
+        #             draw.line([vertex[12], vertex[13], vertex[14], vertex[15]], fill=128, width=2)
 
-                    # 长度方向
-                    # 0-3 1-2 4-7 5-6
-                    draw.line([vertex[0], vertex[1], vertex[6], vertex[7]], fill=128, width=2)
-                    draw.line([vertex[2], vertex[3], vertex[4], vertex[5]], fill=128, width=2)
-                    draw.line([vertex[8], vertex[9], vertex[14], vertex[15]], fill=128, width=2)
-                    draw.line([vertex[10], vertex[11], vertex[12], vertex[13]], fill=128, width=2)
+        #             # 长度方向
+        #             # 0-3 1-2 4-7 5-6
+        #             draw.line([vertex[0], vertex[1], vertex[6], vertex[7]], fill=128, width=2)
+        #             draw.line([vertex[2], vertex[3], vertex[4], vertex[5]], fill=128, width=2)
+        #             draw.line([vertex[8], vertex[9], vertex[14], vertex[15]], fill=128, width=2)
+        #             draw.line([vertex[10], vertex[11], vertex[12], vertex[13]], fill=128, width=2)
 
-                    # 高度方向
-                    # 0-4 1-5 2-6 3-7
-                    draw.line([vertex[0], vertex[1], vertex[8], vertex[9]], fill=128, width=2)
-                    draw.line([vertex[2], vertex[3], vertex[10], vertex[11]], fill=128, width=2)
-                    draw.line([vertex[4], vertex[5], vertex[12], vertex[13]], fill=128, width=2)
-                    draw.line([vertex[6], vertex[7], vertex[14], vertex[15]], fill=128, width=2)
-                # del draw
+        #             # 高度方向
+        #             # 0-4 1-5 2-6 3-7
+        #             draw.line([vertex[0], vertex[1], vertex[8], vertex[9]], fill=128, width=2)
+        #             draw.line([vertex[2], vertex[3], vertex[10], vertex[11]], fill=128, width=2)
+        #             draw.line([vertex[4], vertex[5], vertex[12], vertex[13]], fill=128, width=2)
+        #             draw.line([vertex[6], vertex[7], vertex[14], vertex[15]], fill=128, width=2)
+        #         # del draw
 
-                plt.subplot(2,2,1)
-                plt.imshow(pil_raw_img)  # RGB
+        #         plt.subplot(2,2,1)
+        #         plt.imshow(pil_raw_img)  # RGB
 
-                # 绘制热力图
-                plt.subplot(2,2,2)
-                hotmaps = batch_hm[num][...,0]
-                print(hotmaps.shape)
-                heatmap = np.maximum(hotmaps, 0)
-                heatmap /= np.max(heatmap)
-                plt.imshow(heatmap)
+        #         # 绘制热力图
+        #         plt.subplot(2,2,2)
+        #         hotmaps = batch_hm[num][...,0]
+        #         print(hotmaps.shape)
+        #         heatmap = np.maximum(hotmaps, 0)
+        #         heatmap /= np.max(heatmap)
+        #         plt.imshow(heatmap)
 
-                # 将灰度图转换为伪彩色图
-                plt.subplot(2,2,3)
-                heatmap = cv.resize(heatmap, (512, 512))
-                heatmap = np.uint8(255 * heatmap)
-                heatmap = cv.applyColorMap(heatmap, cv.COLORMAP_JET)
-                raw_img = cv.cvtColor(raw_img, cv.COLOR_RGB2BGR)
-                superimposed_img = heatmap * 0.4 + raw_img * 0.8  # BGR
+        #         # 将灰度图转换为伪彩色图
+        #         plt.subplot(2,2,3)
+        #         heatmap = cv.resize(heatmap, (512, 512))
+        #         heatmap = np.uint8(255 * heatmap)
+        #         heatmap = cv.applyColorMap(heatmap, cv.COLORMAP_JET)
+        #         raw_img = cv.cvtColor(raw_img, cv.COLOR_RGB2BGR)
+        #         superimposed_img = heatmap * 0.4 + raw_img * 0.8  # BGR
 
-                cv.imwrite("img/hot.jpg", superimposed_img)
-                superimposed_img = np.array(superimposed_img, dtype=np.uint8)
-                superimposed_img = cv.cvtColor(superimposed_img, cv.COLOR_BGR2RGB)
-                plt.imshow(superimposed_img)
+        #         cv.imwrite("img/hot.jpg", superimposed_img)
+        #         superimposed_img = np.array(superimposed_img, dtype=np.uint8)
+        #         superimposed_img = cv.cvtColor(superimposed_img, cv.COLOR_BGR2RGB)
+        #         plt.imshow(superimposed_img)
 
-            plt.show()
+        #     plt.show()
 
 
         for epoch in range(Init_Epoch,Freeze_Epoch):
@@ -364,4 +369,11 @@ if __name__ == "__main__":
         for epoch in range(Freeze_Epoch,Unfreeze_Epoch):
             val_loss = fit_one_epoch(net,epoch,epoch_size,epoch_size_val,gen,gen_val,Unfreeze_Epoch,Cuda, writer)
             lr_scheduler.step(val_loss)
+            
+            early_stopping(val_loss, net)
+            # 若满足 early stopping 要求
+            if early_stopping.early_stop:
+                print("Early stopping")
+                # 结束模型训练
+                break
 
